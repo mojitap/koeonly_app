9417cd33b138bd0cf8915968618a7ee347e6706a
mojitap <ta714kadvance@gmail.com>
2025-07-12 14:38:03 +0900

    Add files via upload

---
+++ maleIcons.js
@@ -0,0 +1,11 @@
+import icon1 from "/src/assets/icons/male1.png";
+import icon2 from "/src/assets/icons/male2.png";
+import icon3 from "/src/assets/icons/male3.png";
+import icon4 from "/src/assets/icons/male4.png";
+import icon5 from "/src/assets/icons/male5.png";
+import icon6 from "/src/assets/icons/male6.png";
+import icon7 from "/src/assets/icons/male7.png";
+import icon8 from "/src/assets/icons/male8.png";
+import icon9 from "/src/assets/icons/male9.png";
+
+export default [icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8, icon9];
