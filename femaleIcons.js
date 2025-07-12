90c1e12be1274bba0fe8621276c5a01b9955a9ea
mojitap <ta714kadvance@gmail.com>
2025-07-12 14:19:51 +0900

    Add files via upload

---
+++ femaleIcons.js
@@ -0,0 +1,11 @@
+import icon1 from "/src/assets/icons/female1.png";
+import icon2 from "/src/assets/icons/female2.png";
+import icon3 from "/src/assets/icons/female3.png";
+import icon4 from "/src/assets/icons/female4.png";
+import icon5 from "/src/assets/icons/female5.png";
+import icon6 from "/src/assets/icons/female6.png";
+import icon7 from "/src/assets/icons/female7.png";
+import icon8 from "/src/assets/icons/female8.png";
+import icon9 from "/src/assets/icons/female9.png";
+
+export default [icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8, icon9];
